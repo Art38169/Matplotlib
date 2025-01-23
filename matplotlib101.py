c9a3828c716343ab840fdfb904347b987bf915f7
Art38169 <86408487+Art38169@users.noreply.github.com>
2025-01-23 11:54:01 +0700

    Data Bars

--- matplotlib101.py
+++ matplotlib101.py
@@ -20,20 +20,31 @@ import numpy as np
 # plt.show()
 
 
-fig = plt.figure(figsize = (8, 6))
-ax = fig.add_subplot(111, projection = '3d')
+# fig = plt.figure(figsize = (8, 6))
+# ax = fig.add_subplot(111, projection = '3d')
     
-x_vals = np.linspace(-3, 3, 50)
-y_vals = np.linspace(-3, 3, 50)
-X, Y = np.meshgrid(x_vals, y_vals)
-Z = np.sin(3 * X) * Y
+# x_vals = np.linspace(-3, 3, 50)
+# y_vals = np.linspace(-3, 3, 50)
+# X, Y = np.meshgrid(x_vals, y_vals)
+# Z = np.sin(3 * X) * Y
     
-ax.plot_surface(X, Y, Z, cmap = 'viridis', edgecolor = 'green')
-ax.set_title('Surface Plot: z = y * sin(3x)')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
-ax.grid(True)
+# ax.plot_surface(X, Y, Z, cmap = 'viridis', edgecolor = 'green')
+# ax.set_title('Surface Plot: z = y * sin(3x)')
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.set_zlabel('Z')
+# ax.grid(True)
+# plt.show()
+
+data_bars = np.loadtxt('values_for_bars.csv', delimiter=' ')
+# data_hist = np.loadtxt('values_for_hist.csv', delimiter=' ')
+unique_values, counts = np.unique(data_bars, return_counts=True)
+
+plt.bar(unique_values, counts, color='orange', width=0.6)
+
+plt.title('Bar Chart of Unique Values')
+plt.xlabel('Unique Values')
+plt.ylabel('Frequency')
+
 plt.show()
-
-
+print(data_bars)
